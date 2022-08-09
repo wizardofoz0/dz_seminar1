469aedcd2366a2b7785edfa28a275853e80130d4
Valpeters Marina <mvalpeters@gmail.com>
2022-08-09 23:25:28 +0300

    zadacha_8

---
+++ zadacha_8/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int [] OddNumber(int maxnumber)
+{   int size = maxnumber/2;
+    int [] numbers = new int[size];
+    int S = 0;
+  //  Console.WriteLine($" size = {size}");
+    for (int i = 0; i < size; i++) 
+    {
+        S = S + 2;
+        numbers[i] = S;
+      //  Console.WriteLine($" i = {i}");
+      //  Console.WriteLine($" numbers = {numbers[i]}");
+    }
+    return numbers;
+  }
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int[] numbers = OddNumber(number);
+for (int i = 0; i < numbers.Length; i++) 
+    {
+        Console.WriteLine(numbers[i]);
+    }
+
